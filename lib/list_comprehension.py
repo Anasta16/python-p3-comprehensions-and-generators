b61227262266032c3e919c995f43d5172bfb4f70
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2023-02-20 16:17:51 -0500

    list comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = []
+    for n in num_list:
+        if n % 2 == 0:
+            even_numbers.append(n)
+    return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = []
+    for i in sentence_list:
+        new_list = [i + '!' for i in sentence_list]
+    return new_list
